9aa3a16d11c642c702d4a39bbbfa31050012fb5d
nuriagrinyo <nuriagrinyobielsa@gmail.com>
2021-10-28 13:12:25 +0200

    ex8 plantejament

--- js/checkout.js
+++ js/checkout.js
@@ -11,4 +11,23 @@ var errorPhone = document.getElementById('errorPhone');
 // Exercise 8
 function validate() {
     // Validate fields entered by the user: name, phone, password, and email
+
+    return false;
 }
+
+
+/* 
+- Tots els camps són obligatoris.
+
+- Tots els camps han de tenir almenys 3 caràcters.
+
+- El nom i cognoms han de contenir només lletres.
+
+- El telèfon ha de contenir només números.
+
+- La contrasenya ha d'incloure números i lletres.
+
+- L'email ha de tenir format d'email.
+
+
+*/
