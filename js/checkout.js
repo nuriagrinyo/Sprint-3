c923c15a7ddb838b1f8caacf38edda070b855bf8
nuriagrinyo <nuriagrinyobielsa@gmail.com>
2021-11-01 20:39:18 +0100

    ex8 desenvolupament

--- js/checkout.js
+++ js/checkout.js
@@ -1,33 +1,71 @@
+
+/*
 // Get the input fields
 var password = document.querySelector(".password");
 var phone = document.querySelector('.phone');
-var name = document.querySelector('.name');
+var nom = document.querySelector('.name');
 
 // Get the error elements
 var errorPassword = document.getElementById("errorPassword");
 var errorName = document.getElementById('errorName');  
-var errorPhone = document.getElementById('errorPhone');  
+var errorPhone = document.getElementById('errorPhone');
+*/
 
-// Exercise 8
+
+const form = document.getElementById("form");
+const firstName = document.getElementById("firstNname");
+const email = document.getElementById("email");
+const adress = document.getElementById("adress");
+const lastName = document.getElementById("lastName");
+const password = document.getElementById("password");
+const phone = document.getElementById("phone");
+
+
+
+
+
+
 function validate() {
-    // Validate fields entered by the user: name, phone, password, and email
+
+    const firstNameValue = firstName.value.trim();
+    const emailValue = email.value.trim();
+    const adressValue = adress.value.trim();
+    const lastNameValue = lastName.value.trim();
+    const passwordValue = password.value.trim();
+    const phoneValue = phone.value.trim();
+
+   
+    if (firstName === " ") {
+        // show error
+        //add error class
+        console.log("error");
+        //setErrorFor(firstName, "El nom no pot estar buit");
+
+    } else {
+        // success class
+        setSuccessFor(firstName);
+
+    }
+
 
     return false;
+
+
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+
+
 }
 
 
 /* 
-- Tots els camps són obligatoris.
 
-- Tots els camps han de tenir almenys 3 caràcters.
-
-- El nom i cognoms han de contenir només lletres.
-
-- El telèfon ha de contenir només números.
 
 - La contrasenya ha d'incloure números i lletres.
 
-- L'email ha de tenir format d'email.
+
 
 
 */
