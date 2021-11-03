362ecce37867da03749f3b29404bed465c3152ff
nuriagrinyo <nuriagrinyobielsa@gmail.com>
2021-11-03 11:48:36 +0100

    ex8 meitat

--- js/checkout.js
+++ js/checkout.js
@@ -15,7 +15,13 @@ var errorPhone = document.getElementById('errorPhone');*/
 var form = document.getElementById("form");
 
 var nom = document.getElementById("nom");
+var email = document.getElementById("email");
+var adress = document.getElementById("adress");
+var cognom = document.getElementById("cognom");
+var password = document.getElementById("password");
+var phone = document.getElementById("phone");
 
+var input = document.getElementsByTagName("input");
 
 
 
@@ -27,24 +33,104 @@ form.addEventListener("submit", (e) =>  {
 
 
 
+
 function validate() {
 
     // get the values from the inputs
     var nomValue = nom.value;
+    var emailValue = email.value;
+    var adressValue = adress.value;
+    var cognomValue = cognom.value;
+    var passwordValue = password.value;
+    var phoneValue = phone.value;
 
+    console.log(nomValue);
+
+   
+
+
+
+
+    //TOTS ELS CAMPS SON OBLIGATORIS
+    // NOM
     if (nomValue === '' || nomValue === null) {
-        //show error
-        //add error class
         setErrorFor(nom, "Cal escriure un nom");
-
-    } else {
-        //add success class
+    } else if (!comprovarLletres(nomValue)) {
+        setErrorFor(nom, "Només lletres al nom");
+    }
+    else {
         setSuccessFor(nom);
     }
 
+    // EMAIL
+    if (emailValue === '' || emailValue === null) {
+        setErrorFor(email, "Cal escriure un email");
+    } else if (!comprovarEmail(emailValue)) {
+        setErrorFor(email, "S'ha d'introduir un email vàlid");
+    } else {
+        setSuccessFor(email);
+    }
+
+    // ADRESS
+    if (adressValue === '' || adressValue === null) {
+        setErrorFor(adress, "Cal escriure una adreça");
+    } else {
+        setSuccessFor(adress);
+    }
+
+    // COGNOM
+    if (cognomValue === '' || cognomValue === null) {
+        setErrorFor(cognom, "Cal escriure un cognom");
+    } else if (!comprovarLletres(cognomValue)) {
+        setErrorFor(cognom, "Només lletres al cognom");
+    } else {
+        setSuccessFor(cognom);
+    }
+
+    // PASSWORD
+    if (passwordValue === '' || passwordValue === null) {
+        setErrorFor(password, "Cal escriure una contrassenya");
+    } else if (!comprovarLletres(passwordValue)) {
+        setErrorFor(password, "La contrasenya ha de tenir números i lletres");
+    } else if (!comprovarNumeros(passwordValue)) {
+        setErrorFor(password, "La contrasenya ha de tenir números i lletres");
+    } else {
+        setSuccessFor(password);
+    }
+
+    // PHONE
+    if (phoneValue === '' || phoneValue === null) {
+        setErrorFor(phone, "Cal escriure una contrassenya");
+    } else if (!comprovarNumeros(phoneValue)) {
+        setErrorFor(phone, "Només números al telèfon");
+
+    } else {
+        setSuccessFor(phone);
+    }
+
+
+
     
 
+
+
+ 
+    
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
 
 
 function setErrorFor(input, message) {
@@ -65,3 +151,19 @@ function setSuccessFor(input) {
 
 }
 
+function comprovarEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+
+}
+
+function comprovarLletres(nom, cognom) {
+    return /^[A-Za-z]+$/.test(nom, cognom);
+}
+
+function comprovarNumeros(phone) {
+    return /^[0-9]+$/.test(phone);
+}
+
+function comprovarLletresNumeros (password) {
+    return /^[0-9a-zA-Z]+$/.test(password);
+}
