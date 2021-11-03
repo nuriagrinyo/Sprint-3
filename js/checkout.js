11407072411832f27c72d9ced65e24a966e963e4
nuriagrinyo <nuriagrinyobielsa@gmail.com>
2021-11-03 13:25:03 +0100

    ex8 js final

--- js/checkout.js
+++ js/checkout.js
@@ -1,19 +1,6 @@
 
-/*
-// Get the input fields
-var password = document.querySelector(".password");
-var phone = document.querySelector('.phone');
-var nom = document.querySelector('.name');
-
-// Get the error elements
-var errorPassword = document.getElementById("errorPassword");
-var errorName = document.getElementById('errorName');  
-var errorPhone = document.getElementById('errorPhone');*/
-
-
-
+//Declaració variables
 var form = document.getElementById("form");
-
 var nom = document.getElementById("nom");
 var email = document.getElementById("email");
 var adress = document.getElementById("adress");
@@ -21,22 +8,14 @@ var cognom = document.getElementById("cognom");
 var password = document.getElementById("password");
 var phone = document.getElementById("phone");
 
-var input = document.getElementsByTagName("input");
-
-
-
-
 form.addEventListener("submit", (e) =>  {
         e.preventDefault();
         validate();
 })
 
-
-
-
 function validate() {
 
-    // get the values from the inputs
+    //get the values from the inputs
     var nomValue = nom.value;
     var emailValue = email.value;
     var adressValue = adress.value;
@@ -44,126 +23,106 @@ function validate() {
     var passwordValue = password.value;
     var phoneValue = phone.value;
 
-    console.log(nomValue);
-
-   
-
-
-
-
-    //TOTS ELS CAMPS SON OBLIGATORIS
     // NOM
     if (nomValue === '' || nomValue === null) {
-        setErrorFor(nom, "Cal escriure un nom");
+        setErrorFor(nom, "Name cannot be blank");
     } else if (!comprovarLletres(nomValue)) {
-        setErrorFor(nom, "Només lletres al nom");
-    }
-    else {
+        setErrorFor(nom, "Only letters are accepted");
+    } else if (!comprovarCaracters(nomValue)) {
+        setErrorFor(nom, "Minimum of 3 characters");
+    } else {
         setSuccessFor(nom);
     }
 
     // EMAIL
     if (emailValue === '' || emailValue === null) {
-        setErrorFor(email, "Cal escriure un email");
+        setErrorFor(email, "Emial cannot be blank");
     } else if (!comprovarEmail(emailValue)) {
-        setErrorFor(email, "S'ha d'introduir un email vàlid");
+        setErrorFor(email, "Please, enter a valid email");
     } else {
         setSuccessFor(email);
     }
 
     // ADRESS
     if (adressValue === '' || adressValue === null) {
-        setErrorFor(adress, "Cal escriure una adreça");
+        setErrorFor(adress, "Adress cannot be blank");
+    } else if (!comprovarCaracters(adressValue)) {
+        setErrorFor(adress, "Minimum of 3 characters");
     } else {
         setSuccessFor(adress);
     }
 
     // COGNOM
     if (cognomValue === '' || cognomValue === null) {
-        setErrorFor(cognom, "Cal escriure un cognom");
+        setErrorFor(cognom, "Last name cannot be blank");
     } else if (!comprovarLletres(cognomValue)) {
-        setErrorFor(cognom, "Només lletres al cognom");
+        setErrorFor(cognom, "Only letters are accepted");
+    } else if (!comprovarCaracters(cognomValue)) {
+        setErrorFor(cognom, "Minimum of 3 characters");
     } else {
         setSuccessFor(cognom);
     }
 
     // PASSWORD
     if (passwordValue === '' || passwordValue === null) {
-        setErrorFor(password, "Cal escriure una contrassenya");
-    } else if (!comprovarLletres(passwordValue)) {
-        setErrorFor(password, "La contrasenya ha de tenir números i lletres");
-    } else if (!comprovarNumeros(passwordValue)) {
-        setErrorFor(password, "La contrasenya ha de tenir números i lletres");
+        setErrorFor(password, "Password cannot be blank");
+    } else if (!comprovarPassword(passwordValue)) {
+        setErrorFor(password, "Letters and numbers are required, and 4-8 characters");
+
     } else {
         setSuccessFor(password);
     }
 
     // PHONE
     if (phoneValue === '' || phoneValue === null) {
-        setErrorFor(phone, "Cal escriure una contrassenya");
+        setErrorFor(phone, "Phone cannot be blank");
     } else if (!comprovarNumeros(phoneValue)) {
-        setErrorFor(phone, "Només números al telèfon");
-
+        setErrorFor(phone, "Only numbers are accepted");
+    }  else if (!comprovarCaracters(phoneValue)) {
+    setErrorFor(phone, "Minimum of 3 characters");
     } else {
         setSuccessFor(phone);
     }
+} // final funció validate
 
 
 
-    
-
-
-
- 
-    
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
+//Missatge d'error
 function setErrorFor(input, message) {
     var formControl = input.parentElement; //form-control
     var small = formControl.querySelector("small");
 
-    //add error message inside small
     small.innerText = message;
-
-    //add error class
     formControl.className = "form-control error";
-
 }
 
+//Missatge de validació
 function setSuccessFor(input) {
     var formControl = input.parentElement; //form-control
     formControl.className = "form-control success";
-
 }
 
+//Comprovar que l'input té un mínim de 3 caràcter (siguin lletres o números)
+function comprovarCaracters(nom, email, adress, cognom, phone) {
+    return /^[a-zA-Z0-9]{3,}$/.test(nom, email, adress, cognom, phone);
+}
+
+//Comprovar si l'email és vàlid
 function comprovarEmail(email) {
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-
 }
 
+//Comprovar que l'input només conté lletres
 function comprovarLletres(nom, cognom) {
     return /^[A-Za-z]+$/.test(nom, cognom);
 }
 
+//Comprovar que l'input només conté números
 function comprovarNumeros(phone) {
     return /^[0-9]+$/.test(phone);
 }
 
-function comprovarLletresNumeros (password) {
-    return /^[0-9a-zA-Z]+$/.test(password);
+//Comprovar que la contrasenya conté números i lletres i entre 4 i 8 caràcters
+function comprovarPassword(password) {
+    return /^(?=.*\d)(?=.*[a-zA-Z]).{4,8}$/.test(password);
 }
